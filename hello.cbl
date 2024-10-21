ee9e8ef4d0fdbf3477f8f96d62f065c29ecf94ff
Vallikannu Lakshmanan <vallikannu.l@royalcyber.com>
2024-10-20 18:32:34 -0700

    Commit on StoryABC

--- hello.cbl
+++ hello.cbl
@@ -4,7 +4,7 @@
 000400*     test123                                                            0
 000500*    THIS PROGRAM WILL RECEIVE A DATE AND COVERT THE DATE TO      00000500
 000600*    AN INTEGER IN A CALLED PROGRAM TO DETERMINE DAYS FROM        00000600
-000700*    CURRENT DATEe.                                               00000700
+000700*    CURRENT DATEE.                                               00000700
 000800*                                                                 00000800
 000900*    (C) 2017 IBM - JIM HILDNER RESERVED.                         00000900
 001000 ENVIRONMENT DIVISION.                                            00001000
