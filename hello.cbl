59f360f405f055660ad43735a9defe8a2501379c
Vallikannu Lakshmanan <vallikannu.l@royalcyber.com>
2024-10-20 19:39:21 -0700

    CmtNew

--- hello.cbl
+++ hello.cbl
@@ -3,7 +3,7 @@
 000300*    THIS IS SAMPLE FOR GIT/DBB TRAINING STORY123- EPSDEMOS 2008    000003
 000400*     test123                                                            0
 000500*    TtIS PROGRAM WILL RECEIVE A DATE AND COVERT THE DATE TO      00000500
-000600*    AN INTEGER IN A CALLED PROGRAM TO DETERMINE DAYS FROM        00000600
+000600*    AN INTEGER IN A CALLED PROGRAM TO DETERMINE DAYS FROM THE    00000600
 000700*    CURRENT DATIE.                                               00000700
 000800*                                                                 00000800
 000900*    (C) 2017 IBM - JIM HILDNER RESERVEDD.                        00000900
